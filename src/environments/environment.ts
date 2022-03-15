f06bdf03d0a23bc6a04a8290974a52e2cb018889
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-03-15 16:01:44 -0600

    install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'qr-admin-a1573',
+    appId: '1:42737786083:web:b3fdb85c8bc9d38f50b9d6',
+    storageBucket: 'qr-admin-a1573.appspot.com',
+    apiKey: 'AIzaSyC8EVp98Bt_mwXC_gtJCovC58d_8cYcZRs',
+    authDomain: 'qr-admin-a1573.firebaseapp.com',
+    messagingSenderId: '42737786083',
+    measurementId: 'G-N2X9BRCXMD',
+  },
+  production: false,
 };
 
 /*
