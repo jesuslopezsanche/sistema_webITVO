52ae328e8a7931add8ba068ce2378c1926a6a619
Rebjai <rebjai@github.com>
2022-03-15 16:01:44 -0600

    install firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'qr-admin-a1573',
+    appId: '1:42737786083:web:b3fdb85c8bc9d38f50b9d6',
+    storageBucket: 'qr-admin-a1573.appspot.com',
+    apiKey: 'AIzaSyC8EVp98Bt_mwXC_gtJCovC58d_8cYcZRs',
+    authDomain: 'qr-admin-a1573.firebaseapp.com',
+    messagingSenderId: '42737786083',
+    measurementId: 'G-N2X9BRCXMD',
+  },
   production: true
 };
