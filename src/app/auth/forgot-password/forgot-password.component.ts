691139a3f2511c47e68c4177d2a9a01985787edc
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-03-16 00:35:45 -0600

    forgot password page / password reset

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  resetPasswordForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.resetPasswordForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]]
+      }
+    )
+  }
   ngOnInit(): void {
+  }
+  async resetPassword(){
+    if (this.resetPasswordForm.invalid) {
+      return 
+    }
+    let res = await this.authService.resetPassword(this.resetPasswordForm.get("email")?.value)
+    if (res == undefined) {
+      alert('Se ha enviado un link para reestablecer tu contraseña a tu correo')
+    }
+    
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Router } from '@angular/router';
 import { Observable, of, switchMap, take } from 'rxjs';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, authState, updateProfile } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, authState, updateProfile, sendPasswordResetEmail } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import firebase from "firebase/compat/app";
-import { doc, docData, Firestore} from '@angular/fire/firestore';
+import { doc, docData, Firestore } from '@angular/fire/firestore';
 export interface User {
   uid: string
   email: string
@@ -19,42 +19,41 @@ export class AuthService {
   constructor(private auth: Auth, private router: Router, private firestore: Firestore) {
     this.userLoggedIn = false
     this.user$ = of(null)
-    authState(this.auth).subscribe((u) => 
-    {
-      
+    authState(this.auth).subscribe((u) => {
+
       if (u) {
-        
+
         this.user$ = of({
           displayName: u?.displayName,
           email: u.email,
           uid: u.uid
-          
+
         }) as Observable<User>
         this.router.navigateByUrl(this.redirectUrl)
-        console.log({u});
-              let docu :any 
-              docData(doc(this.firestore, `users/${u.uid}`)).subscribe(e =>{
-                console.log(e);
-                docu = e
-                
-              })
-              console.log({docu});
+        console.log({ u });
+        let docu: any
+        docData(doc(this.firestore, `users/${u.uid}`)).subscribe(e => {
+          console.log(e);
+          docu = e
+
+        })
+        console.log({ docu });
         return
-        
+
       }
       this.user$ = of(null)
 
     }
-      )
+    )
     console.log(this.user$);
-    
+
     this.auth.onAuthStateChanged((loggedUser) => {
       if (loggedUser) {
         this.userLoggedIn = true
         this.user$ = of({ uid: loggedUser.uid, email: loggedUser.email!, displayName: loggedUser.displayName! })
         this.userLoggedIn = true
         this.router.navigateByUrl(this.redirectUrl)
-        
+
       } else {
         this.userLoggedIn = false
         this.user$ = of(null)
@@ -66,31 +65,36 @@ export class AuthService {
     return createUserWithEmailAndPassword(this.auth, user.email, user.password)
       .then(r => {
         let emailLower = user.email.toLowerCase();
-        
+
         console.log(r.user.emailVerified)
-        
-      }).catch(e=> e as string)
+
+      }).catch(e => e as string)
   }
 
   async loginEmail(user: any): Promise<any> {
 
     try {
-      
-      
+
+
       console.log(user);
       let login = await signInWithEmailAndPassword(this.auth, user.email, user.password)
       console.log(login.user);
       this.userLoggedIn = true
       this.router.navigate(['students'])
     } catch (error) {
-      console.log('error',error);
+      console.log('error', error);
       return error
-      
+
+    }
+
+  }
+  async resetPassword(email:string) {
+    let response = await sendPasswordResetEmail(this.auth, email)
+    console.log(response)
+    this.router.navigate(['auth'])
+    
   }
 
-
-  }
-  
   logout() {
     this.auth.signOut()
     this.userLoggedIn = false
