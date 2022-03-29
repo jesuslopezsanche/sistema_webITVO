423bb7df50d0aa156af7cd4a97df53822cf61bfd
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-03-29 04:27:11 -0600

    top materials initial scaffold

--- src/app/admin/materials/materials-routing.module.ts
+++ src/app/admin/materials/materials-routing.module.ts
@@ -1,3 +1,4 @@
+import { TopMaterialsComponent } from './top-materials/top-materials.component';
 import { MaterialFormComponent } from './material-form/material-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,7 @@ import { MaterialsComponent } from './materials.component';
 const routes: Routes = [
   {path: '', children:[
     {path:'', component: MaterialsComponent},
+    {path:'top-herramientas', component: TopMaterialsComponent},
     {path:'create', component: MaterialFormComponent},
     {path:'edit/:id', component: MaterialFormComponent},
     {path:'**', redirectTo: '', pathMatch: 'full'},
