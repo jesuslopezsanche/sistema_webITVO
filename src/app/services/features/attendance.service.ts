b7cdc3eec45dcf5229ff6faf6e97e4a024750345
Rebjai <rebjai@github.com>
2022-05-06 12:16:48 -0500

    QR module integration

--- src/app/services/features/attendance.service.ts
+++ src/app/services/features/attendance.service.ts
@@ -4,13 +4,13 @@ import { Firestore, DocumentData, QueryDocumentSnapshot, getDocs, where, collect
 import { Injectable } from '@angular/core';
 import { from, Observable, of, switchMap, take } from 'rxjs';
 
-export interface Attendance{
+export interface Attendance {
   id?: Area,
   area: Area,
   controlNumber: string,
   name: string,
   carrer: string,
-  startDateTime:string,
+  startDateTime: string,
   endDateTime: string,
 }
 
@@ -30,17 +30,17 @@ export class AttendanceService {
   }
   getAll() {
     let sessions = getDocs(query(this.colRef, where('status', '!=', 'closed')))
-    .then( e =>e.docs)
-    .then(e => e.map(el => el.data()))
+      .then(e => e.docs)
+      .then(e => e.map(el => el.data()))
     return from(sessions)
   }
   getAllfromStudent() {
-    
+
     let sessions = this.authService.user$.pipe(
       switchMap(async user => {
         if (user)
-        return await (await getDocs(query(this.colRef, where('student.uid', '==', this.uid), where('status', '!=', 'closed')))).docs
-        .map(e => e.data())
+          return await (await getDocs(query(this.colRef, where('student.uid', '==', this.uid), where('status', '!=', 'closed')))).docs
+            .map(e => e.data())
         return null
       }
       )
@@ -51,4 +51,20 @@ export class AttendanceService {
   create(area: Attendance) {
     return from(addDoc(this.colRef, area).then(e => e))
   }
+  async registerStudent(student: string) {
+    let time = await fetch('https://worldtimeapi.org/api/timezone/America/Mexico_city',
+      {
+        headers: {
+        //   'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        method: 'GET',
+        // mode:'cors'
+      })
+      time = await time.json()
+    console.log({ timea: await time.json()});
+    // addDoc(this.colRef,{student_id: student, startHour: time)
+
+  }
+
 }
