46c8fcfe74b3a39e7a8b0f71eadb7d6d9643734e
Rebjai <15723342+Rebjai@users.noreply.github.com>
2022-06-27 04:46:51 -0500

    tooltip implementation

--- src/app/admin/areas/report/report.component.ts
+++ src/app/admin/areas/report/report.component.ts
@@ -4,7 +4,9 @@ import { Attendance, AttendanceService } from './../../../services/features/atte
 import { AreaService } from './../../../services/features/area.service';
 import { ChartType, ChartEvent } from 'ng-chartist';
 import { Component, OnInit } from '@angular/core';
-import { IChartistData, IPieChartOptions } from 'chartist';
+import { IChartistData, IPieChartOptions, plugins } from 'chartist';
+import * as Chartist from 'chartist';
+import 'chartist-plugin-tooltips';
 
 @Component({
   selector: 'app-report',
@@ -24,7 +26,8 @@ export class ReportComponent implements OnInit {
           return a + b
         })
       return val + ': ' + (Math.round((<number>this.chartData.series[i] / sum) * 100)) + '%'
-    }
+    },
+    plugins: [Chartist.plugins.tooltip()],
   }
   chart2Options: IPieChartOptions = {
     width: '100%', height: '55vh',
@@ -34,13 +37,14 @@ export class ReportComponent implements OnInit {
           return a + b
         })
       return val + ': ' + (Math.round((<number>this.careerChartData.series[i] / sum) * 100)) + '%'
-    }
+    },plugins: [Chartist.plugins.tooltip()],
   }
   startDate: string = ''
   endDate: string = ''
   attendance?: Attendance[]
   careers?: Career[]
   selectedCareer = ''
+  labels = [{ name: '', color: '', value: 0 }]
   constructor(private attendanceService: AttendanceService, private areaService: AreaService, private careerService: CareerService) { }
 
   ngOnInit(): void {
@@ -56,13 +60,23 @@ export class ReportComponent implements OnInit {
           return
         }
         let orderedByArea = areas?.map(area => ({ label: area.name, size: r.filter(a => a.area.id == area.id).length }))
-          .sort((a, b) => a.size - b.size)
+          .sort((a, b) => b.label.localeCompare(a.label))
         console.log({ orderedByArea });
 
         this.chartData = {
           labels: orderedByArea!.map(r => r.label),
-          series: orderedByArea!.map(r => r.size ? r.size : 0.0001),
+          series: orderedByArea!.map(r => r.size),
         }
+        this.labels = (<[string]>this.chartData.labels)
+          .map((e: string, i: number) => (
+            {
+              name: e,
+              color: this.getColorClass(i),
+              value: <number>this.chartData.series[i]
+            }
+          ))
+        console.log({ labels: this.labels });
+
         this.attendance = r
       })
 
@@ -77,14 +91,15 @@ export class ReportComponent implements OnInit {
           }
           let orderedByArea = areas!.
             map(area => ({ label: area.name, size: r.filter(a => a.area.id == area.id).length }))
-            .sort((a, b) => a.size - b.size)
+            .sort((a, b) => b.label.localeCompare(a.label))
           console.log({ orderedByArea });
 
 
           this.chartData = {
             labels: orderedByArea!.map(r => r.label),
-            series: orderedByArea!.map(r => r.size ? r.size : 0.0001),
+            series: orderedByArea!.map(r => r.size),
           }
+          this.labels = this.chartData.labels as []
           this.attendance = r
         }
 
@@ -102,14 +117,18 @@ export class ReportComponent implements OnInit {
       let filteredbyCareer = this.attendance?.filter(e => e.student!.career.id == this.selectedCareer)
       console.log({ byCareer: filteredbyCareer });
       let orderedByArea = areas?.map(e => ({ label: e.name, size: filteredbyCareer!.filter(a => a.area.id == e.id).length }))
-        .sort((a, b) => a.size - b.size)
+        .sort((a, b) => b.label.localeCompare(a.label))
       this.careerChartData = {
         labels: orderedByArea!.map(r => r.label),
-        series: orderedByArea!.map(r => r.size ? r.size : 0.0001),
+        series: orderedByArea!.map(r => r.size),
       }
       console.log({ careerData: this.careerChartData });
 
     })
+  }
+  getColorClass(i: number) {
+    return 'ct-series ct-series-' + String.fromCharCode(97 + i)
+
   }
 
 }
